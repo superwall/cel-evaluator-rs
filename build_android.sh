a4400f552875515c71a550bf789af4107cce383f
Ian Rumac <ian.rumac@outlook.com>
2024-11-12 15:44:19 +0100

    Fix android build issue

--- build_android.sh
+++ build_android.sh
@@ -9,11 +9,13 @@ export ANDROID_NDK=r25b ANDROID_SDK=26 ANDROID_VERSION=11.0.0_r48
 build_targets=(
     "x86_64-linux-android"
     "armv7-linux-androideabi"
-    "aarch64-linux-android",
+    "aarch64-linux-android"
     "i686-linux-android"
 )
 
+export CROSS_NO_WARNINGS=0
 for target in "${build_targets[@]}"; do
+    rustup target add "$target"
     echo "Building for $target"
     cross build --target "$target" --release --lib
 done
@@ -23,12 +25,12 @@ echo "Copying results to target/android/jniLibs"
 target_dir="target/android"
 jniLibs_dir="${target_dir}/jniLibs"
 
-mkdir -p "${jniLibs_dir}"/{arm64-v8a,armeabi-v7a,x86_64}
+mkdir -p "${jniLibs_dir}"/{arm64-v8a,armeabi-v7a,x86_64,x86}
 
 cp target/aarch64-linux-android/release/libcel_eval.so "${jniLibs_dir}/arm64-v8a/libuniffi_cel.so"
 cp target/armv7-linux-androideabi/release/libcel_eval.so "${jniLibs_dir}/armeabi-v7a/libuniffi_cel.so"
 cp target/x86_64-linux-android/release/libcel_eval.so "${jniLibs_dir}/x86_64/libuniffi_cel.so"
-cp target/i686-linux-android/release/libcel_eval.so "${jniLibs_dir}/i686/libuniffi_cel.so"
+cp target/i686-linux-android/release/libcel_eval.so "${jniLibs_dir}/x86/libuniffi_cel.so"
 
 echo "Running UniFFI to generate Kotlin bindings"
 mkdir -p "${target_dir}/java/uniffi/cel"
